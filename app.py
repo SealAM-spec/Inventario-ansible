080e9fc693fdf0df2f21eaf3a94f7976d22fe308
Seal_AM <sergioalbam89@gmail.com>
2025-09-23 08:15:07 -0600

    Add: Resta

---
+++ app.py
@@ -0,0 +1,17 @@
+import socket
+print("Hello world")
+
+hostname = socket.gethostname()
+print(f"Hostname: ")
+
+IpAddress = socket.gethostbyname(hostname)
+print(f"IP Address: {IpAddress}")
+
+for i in range(10):
+    print(i)
+    
+a = int(input("Dame el primer numero:"))
+b = int(input("Dame el segundo numero"))
+print(f"La suma de {a} + {b} es igual a {a+b}")
+
+print(f"La resta de {a} - {b} es igual a {a-b}")
