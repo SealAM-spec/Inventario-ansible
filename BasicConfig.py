4c33fdc22dae0cc8b0574d49fe44f495b8e8d9e6
Seal_AM <sergioalbam89@gmail.com>
2025-09-26 10:46:06 -0600

    Initial commit

--- BasicConfig.py
+++ BasicConfig.py
@@ -27,4 +27,23 @@ def configure_device(port, hostname, username, passwrod, domain, baudrate=1600):
     except serial.SerialException as e:
         print(f"❎Error: {e}")
         
+def conectar_router_serial(puerto='COM3', baudios=9600, comando=''):
+    try:
+        with serial.Serial(puerto, baudios, timeout=2) as ser:
+            time.sleep(2)  # Esperar a que el router esté listo
+            ser.write((comando + '\n').encode('utf-8'))
+            time.sleep(1)
+            respuesta = ser.read(ser.in_waiting).decode('utf-8', errors='ignore')
+            print(f"Respuesta del router:\n{respuesta}")
+            return respuesta
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+        
+    def abrir_hoja(port='COM3',baudrate=9600, timeout=1):
+        ser = ser.Serial('COM3', baudrate, timeout=timeout)
+        
 r1 = configure_device("COM3", "R1", "cisco", "cisco", "example.com")
+
+conectar_router_serial(comando='show ip interface brief')
